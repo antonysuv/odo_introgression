ae460b171fe235f125d2b26a594ddc1f4134f3ff
Antons <antony.suvorov@gmail.com>
2019-11-17 21:49:30 -0500

    update

--- phylogeny_prep.r
+++ phylogeny_prep.r
@@ -1,4 +1,10 @@
 library(MCMCtreeR)
+library(ape)
+library(laser)
+library(geiger)
+library(TreeSim)
+library(phytools)
+source("http://ib.berkeley.edu/courses/ib200b/labs/lab12/rbdtree.n3.R")
 
 phy_mcmc=readMCMCtree("/Users/Anton/Downloads/all_dated_trees/run4long.tree.tre")
 phy=phy_mcmc$apePhy
@@ -25,6 +31,48 @@ phy_mcmc$apePhy=phy
 tt=read.table("/Users/Anton/Downloads/mcmc.txt",header=T)
 tt=tt[,1:85]
 
+# Fig 1 Main dated tree
 MCMC.tree.plot(phy_mcmc,analysis.type = "MCMCtree",MCMC.chain =tt,plot.type = "distributions",density.col = adjustcolor( "navy", alpha.f = 0.5),density.border.col = "navy", lwd.bar = 3,scale.res = c("Period"), node.method = "bar",col.age = adjustcolor( "navy", alpha.f = 0.5), no.margin = T, cex.labels = 0.01,cex.tips = 0.6,ladderize.tree = F,pos.age=-7,abs.age.lwd.ticks=0,relative.height=0.05,cex.age = 0.6)
 
 MCMC.tree.plot(phy_mcmc,analysis.type = "MCMCtree",MCMC.chain =tt,plot.type = "cladogram", lwd.bar = 3,scale.res = c("Period"), node.method = "bar",col.age = adjustcolor( "navy", alpha.f = 0.5), no.margin = T, cex.labels = 0.001,cex.tips = 0,ladderize.tree = F,pos.age=-300,abs.age.lwd.ticks=0,relative.height=0.05,cex.age = 0.6, show.tip.label = F)
+
+# Fig 3 LTT
+tt=extract.clade(phy,87)
+yule_odo=yule(tt)
+btimes=sort(branching.times(tt),decreasing=TRUE)
+#83 is the number of tips
+tt_null=replicate(10000,rbdtree.n(83,btimes[1],yule_odo$lambda,0),simplify=FALSE)
+mycol=rgb(190, 190, 190, max = 255, alpha = 10)
+#Read fossil data
+fos=read.csv("/Users/Anton/Desktop/git_repos/macrology/data/pbdb2019_datasets/odonatoptera_pbdb2019.csv" ,sep=",")
+fos=fos[fos$accepted_rank=="species",]
+fos_dens=apply(fos[,c("max_ma","min_ma")],1,mean)
+
+#Plotting
+par(mar = c(5,5,2,5))
+ltt.plot(tt_null[[1]],col=mycol,log="y",xlim=c(-320,0),ylim=c(1,90),ylab="Extant lineages",xlab="Time (Myr ago)")
+for (i in 2:10000)
+{
+        ltt.lines(tt_null[[i]],col=mycol)
+        #gam=c(gam,ltt(tt_null[[i]],log=F,plot=F)$gamma)
+}       
+ltt.lines(tt,col="black",lwd=2)
+lines(-131.9361,10,type="p",pch=16,col="red")
+
+par(new = T)
+hist(-(fos_dens),xlim=c(-320,0),axes=F, xlab=NA, ylab=NA,border=F,nclass=30,col=rgb(138, 163, 255, max = 255, alpha = 90),main="",ylim=c(0,250))
+axis(side = 4)
+mtext(side = 4, line = 3, 'Fossil abundance')
+#P-Tr
+abline(v=-252,lty=2)
+#Tr-J
+abline(v=-201.3,lty=2)
+#K-Pg
+abline(v=-66,lty=2)
+
+mtext(side = 3, "P-Tr",at=-252)
+mtext(side = 3, "Tr-J",at=-201.3)
+mtext(side = 3, "K-Pg",at=-66)
+rect(xleft=-145, ybottom=-10, xright=-66, ytop=0,col=rgb(129,196,85, max = 255))
+text(x=-105.5, y = -5,"Cretaceous",cex=0.9)
+rect(xleft=-145, ybottom=0, xright=-66, ytop=270,col=rgb(129,196,85, max = 255,alpha=40),lty=0)
---
+++ qs_prep.r
@@ -0,0 +1,62 @@
+library(ggplot2)
+library(ape)
+#Quartet Sampling analysis 
+files = list.files(".", pattern="*.scores", full.names=TRUE, recursive=FALSE)
+all_scores=rbind()
+for (f in files)
+{
+    tt=read.csv(f)
+    if (tt[1,1]=="QS1")
+    {
+        tt=tt[3:83,]
+    }
+    else
+    {
+        tt=tt[1:81,]
+    }
+    tt=t(tt)[2:5,]
+    tt=apply(tt,2,as.numeric)
+    row.names(tt)=c("freq0","qc","qd","qi")
+    all_scores=rbind(all_scores,tt)
+}    
+
+#Aniso
+nodes=as.character(c(3,4,5,6,25,37,38,39,62,63,71))
+n_names=c("Anisoptera+\nAnisozygoptera","Aeshnidae+\nExophytica","Cavilabiata+\n(Gomphidae+\nPetaluridae)","Cordulegastroidea+\nLibelluloidea","Gomphidae+\nPetaluridae",
+         "Lestoidea+\n(Platystictoidea+\nCalopterigoidea+\nCoenagrionoidea)","Platystictoidea+\n(Calopterigoidea+\nCoenagrionoidea)","Calopterigoidea+\nCoenagrionoidea","(A+B)+\n(C+D)","A+B","C+D")
+sc=matrix(all_scores[,nodes])
+stat=rep(row.names(all_scores[,nodes]))
+n=as.vector(rep(n_names,each=nrow(all_scores[,nodes])))
+zz=data.frame(score=sc,statistic=stat,node=n)
+
+
+p = ggplot(zz, aes(x=node, y=score,fill=statistic)) + geom_boxplot() + 
+     scale_x_discrete(limits=n_names) +
+     scale_fill_manual(values=c("#999999", "#E69F00", "#56B4E9","white"),name = "Score", labels = c("Frequency", "QC", "QD","QI"))+
+     theme_classic()+
+     labs(x="Node", y = "Score value")+
+     theme(axis.text.x = element_text(angle = 360,face = "bold")) 
+     xlab(1:5)
+
+
+
+phy=read.tree("/Users/Anton/Downloads/RUN_odoqs.labeled.tre")
+phy=drop.tip(phy,"Ephemera_danica")
+phy$node.label=1:83
+brtimes=sort(branching.times(phy),decreasing=T)
+n_order=names(brtimes)[3:83]
+qd=all_scores[row.names(all_scores)=="qd",n_order]
+f=all_scores[row.names(all_scores)=="freq0",n_order]
+qd_min=apply(qd,2,min,na.rm = T)
+qd_max=apply(qd,2,max,na.rm = T)
+
+qd_traj=rbind()
+for (i in 1:1000)
+{
+    mm=apply(rbind(apply(qd,2,function(x){sample(x[!is.na(x)], size = 1)}),apply(qd,2,function(x){sample(x[!is.na(x)], size = 1)})),2,sort)
+    y=runif(81,mm[1,],mm[2,])
+    qd_traj=rbind(qd_traj,y)
+}   
+
+plot(brtimes[3:83],qd_traj[1,],type="l")
+apply(qd_traj,1,lines,type="l",x=brtimes[3:83],col=adjustcolor( "navy", alpha.f = 0.5))
